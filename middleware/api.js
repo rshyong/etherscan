b83b20993b200ae1611ec04c350cbf0ba25e4654
rshyong <rshyong@gmail.com>
2018-09-30 20:33:44 -0400

    adding dotenv

--- middleware/api.js
+++ middleware/api.js
@@ -5,7 +5,8 @@ const fetch = require('node-fetch');
 async function queryEtherscan(req, res, next) {
   if (req.body && !req.body.address) return res.status(400).send('Please provide an ethereum address');
   let address = req.body.address;
-  let url = `https://api.etherscan.io/api?module=proxy&action=eth_getTransactionCount&address=${address}&tag=latest&apikey=IQASWFG9IUX42KGWIYYKCQP68S36EVRXQ1`;
+  let api_key = process.env.API_KEY;
+  let url = `https://api.etherscan.io/api?module=proxy&action=eth_getTransactionCount&address=${address}&tag=latest&apikey=${api_key}`;
   let response = await fetch(url);
   response = await response.json();
   return next();
