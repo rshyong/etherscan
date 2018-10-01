fc7a9a086265cdcd02bfcd0e2c2fecbc87f91335
rshyong <rshyong@gmail.com>
2018-09-30 20:44:44 -0400

    changing endpoint

--- middleware/api.js
+++ middleware/api.js
@@ -4,16 +4,20 @@ const fetch = require('node-fetch');
 
 async function queryEtherscan(req, res, next) {
   if (req.body && !req.body.address) return res.status(400).send('Please provide an ethereum address');
-  let address = req.body.address;
+  let { address, startblock, endblock, } = req.body;
   let api_key = process.env.API_KEY;
-  let url = `https://api.etherscan.io/api?module=proxy&action=eth_getTransactionCount&address=${address}&tag=latest&apikey=${api_key}`;
+  if (!startblock) startblock = 0;
+  if (!endblock) endblock = 99999999;
+  let url = `http://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=${startblock}&endblock=${endblock}&sort=asc&apikey=${api_key}`;
   let response = await fetch(url);
   response = await response.json();
+  req.data = response.result;
   return next();
 }
 
 function sendResponse(req, res) {
-  res.status(200).send({});
+  req.data = req.data || {};
+  res.status(200).send(req.data);
 }
 
 module.exports = {
